1e8af1c20f922c63c1bcb73b21fbb3eb88902e8d
2kurtki <70811661+2kurtki@users.noreply.github.com>
2020-11-08 03:28:37 -0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 500);
+    });
+
+
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('collapse');
+
+        $(blockId).slideToggle('medium', function() {
+            if ($(this).is(':visible'))
+            $(this).css('display','flex');
+        });
+    });
+
+});
